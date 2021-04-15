7bf8e64303caf8b9996ddc16282c98967c19a3fe
bkmais <79587717+bkmais@users.noreply.github.com>
2021-04-16 01:08:22 +0530

    CommitChanges-2

--- palindrome.go
+++ palindrome.go
@@ -1,14 +1,34 @@
 package main
 
-import "strconv"
+func palindrome(x int) bool {
 
-func palindrome(x int) {
-
-	str := strconv.Itoa(x)
-
-	for c := range str {
-
-		println(string(rune(c)))
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
 	}
 
+	revertedNumber := 0
+
+	for x > revertedNumber {
+		revertedNumber = revertedNumber*10 + x%10
+		x = x / 10
+	}
+
+	if x == revertedNumber || x == revertedNumber/10 {
+		return true
+	}
+
+	return false
+}
+
+func isPalindromes(x int) bool {
+	// always return false for negative numer
+	if x < 0 {
+		return false
+	}
+	// normal case: check if x eqauls to its reversed number
+	reversed := 0
+	for tmp := x; tmp != 0; tmp /= 10 {
+		reversed = reversed*10 + tmp%10
+	}
+	return x == reversed
 }
